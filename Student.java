b247a02da7398be98d4ffe8914afbf2d5ae38394
ssroy000 <161429550+ssroy000@users.noreply.github.com>
2024-03-13 20:38:56 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,16 @@
+public class Student {
+    int id;
+    String name;
+
+    public void details(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.details(10,"Susita");
+        System.out.println("Student 1 | ID: "+s1.id+", Name: "+s1.name);
+    }
+}
 
